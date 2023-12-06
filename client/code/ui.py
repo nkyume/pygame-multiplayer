ebd660266443011486f3a8f3af332d584981059b
nkyume <nekoyume2003@gmail.com>
2023-12-06 23:18:04 +1000

    cool update buttons and sprites

--- client/code/ui.py
+++ client/code/ui.py
@@ -47,11 +47,9 @@ class MainMenu():
             self.display_surface.blit(self.loading_text_surf, self.loading_rect)
             if not self.client._connecting:
                 self.state = 'main'
-            
-        
+                    
 class InputBox():
-    def __init__(
-            self,
+    def __init__(self,
             pos: tuple,
             size: tuple,
             font: pg.font.Font,
@@ -91,7 +89,6 @@ class InputBox():
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_v and event.mod & pg.KMOD_CTRL:
                     self.text += pg.scrap.get("text/plain;charset=utf-8").decode()
-                    
                 if event.key == pg.K_BACKSPACE:
                     self.text = self.text[:-1]
                 if event.key == pg.K_RETURN:
@@ -100,11 +97,11 @@ class InputBox():
                     return(text)
 
             # restrict string lenght        
-            if self.max_len:
-                if len(self.text) >= self.max_len:
-                    self.text = self.text[:self.max_len]
             if event.type == pg.TEXTINPUT:
                 self.text += event.text
+        if self.max_len:
+                if len(self.text) >= self.max_len:
+                    self.text = self.text[:self.max_len]
 
         if self.active:
             box_color = self.active_color
@@ -119,5 +116,52 @@ class InputBox():
         pg.draw.rect(display_surf, self.passive_color, self.box_rect)
         pg.draw.rect(display_surf, box_color, self.box_rect, 2)
         display_surf.blit(text_surface, (self.box_rect.x + 5, self.box_rect.y + 7))
+
+class Button():
+    def __init__(self,
+            pos: tuple,
+            size: tuple,
+            text: str,
+            font: pg.font.Font,
+            pressed_color,
+            passive_color,
+            bg_color,
+            font_color, 
+            ):
+        
+        self.pressed_color = pressed_color
+        self.passive_color = passive_color
+        self.bg_color = bg_color
+        self.button_rect = pg.Rect(pos, size)
+        self.button_rect.center = pos
+        
+        self.font = font
+        self.font_color = font_color
+
+        self.text_surf = self.font.render(text, False, font_color)
+        self.text_rect = self.text_surf.get_rect(center=pos)
+        self.button_rect.width = self.text_rect.width
+
+    def draw(self, display_surf, events, func, *args):
+        """run function on click"""
+        mouse_keys = pg.mouse.get_pressed()
+        color = self.passive_color
+        if self.button_rect.collidepoint(pg.mouse.get_pos()):
+            color = self.pressed_color
+            if mouse_keys[0]:
+                func(args)
+            
+                    
+        pg.draw.rect(display_surf, self.bg_color, self.button_rect)
+        display_surf.blit(self.text_surf, self.text_rect)
+        pg.draw.rect(display_surf, color, self.button_rect, 4)
+
+        
+
+
+
+
+
+    
 
         
